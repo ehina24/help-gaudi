027d0559a9f6b5e9d233eeb3383f59d83e62dd58
koudaihirata <2230051@ecc.ac.jp>
2025-11-29 18:39:59 +0900

    update:2回目テストのために変更

--- src/dotTop/mainUi.tsx
+++ src/dotTop/mainUi.tsx
@@ -1,27 +1,28 @@
     import { useState } from "react";
     import DotTop from "./dotTop";
-    import TextBox from "../components/TextBox/TextBox";
     import styles from "./ui.module.css";
+    import Popup from "../components/PopUp";
 
     export default function MainUi() {
     const [showInput, setShowInput] = useState(false);
 
     return (
         <div className={styles.wrapper}>
-        <DotTop />
+            <DotTop />
 
-        <button
-            className={styles.homeruButton}
-            onClick={() => setShowInput(true)}
-        >
-            自分を褒める
-        </button>
+            <button
+                className={styles.homeruButton}
+                onClick={() => setShowInput(true)}
+            >
+                自分を褒める
+            </button>
 
-        {showInput && (
-            <div className={styles.eventInputWrapper}>
-            <TextBox text="カウントされました！昨日も褒められていましたね。積み重ねていきましょう！"/>
-            </div>
-        )}
+            {showInput && (
+                <div className={styles.eventInputWrapper}>
+                    <Popup />
+                    {/* <TextBox text="カウントされました！昨日も褒められていましたね。積み重ねていきましょう！"/> */}
+                </div>
+            )}
         </div>
     );
     }
