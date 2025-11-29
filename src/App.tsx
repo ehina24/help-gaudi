cb55225bc3c618746a3e2f85cbf056b030c68da9
koudaihirata <2230051@ecc.ac.jp>
2025-11-29 22:10:05 +0900

    update:AuthProviderを実装完了

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,35 @@
 import './App.css'
-import {Route, Routes } from "react-router-dom"
+import {Navigate, Route, Routes } from "react-router-dom"
 import MainUi from './dotTop/mainUi'
 import SignUp from './signUp'
 import SignIn from './signIn'
+import { AuthProvider } from './providers/AuthProvider'
+import type { JSX } from 'react'
+import { useAuth } from './hooks/useAuth'
+
+function RequireAuth({ children }: { children: JSX.Element }) {
+  const { user, loading } = useAuth();
+
+  if (loading) return <p>判定中...</p>;
+  if (!user) return <Navigate to="/login" replace />;
+
+  return children;
+}
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<MainUi />}/>
-        <Route path='/signUp' element={<SignUp />}/>
-        <Route path='/signIn' element={<SignIn />}/>
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path='/signUp' element={<SignUp />}/>
+          <Route path='/signIn' element={<SignIn />}/>
+          <Route path="/" element={
+            <RequireAuth>
+              <MainUi />
+            </RequireAuth>
+            }/>
+        </Routes>
+      </AuthProvider>
     </>
   )
 }
