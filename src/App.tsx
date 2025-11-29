69d1e611ccd19160a0226ebe28f581de0cd5b32a
koudaihirata <2230051@ecc.ac.jp>
2025-11-29 20:59:57 +0900

    add:SignIn&SignUpを実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css'
 import {Route, Routes } from "react-router-dom"
 import MainUi from './dotTop/mainUi'
+import SignUp from './signUp'
+import SignIn from './signIn'
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<MainUi />}/>
+        <Route path='/signUp' element={<SignUp />}/>
+        <Route path='/signIn' element={<SignIn />}/>
       </Routes>
     </>
   )
---
+++ src/signIn/index.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { api, type User } from "../api";
+
+
+export default function SignIn() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [msg, setMsg] = useState<string>("");
+    const [loading, setLoading] = useState(false);
+    const [user, setUser] = useState<null | User>(null);
+
+    const onLogin = async () => {
+        setMsg("");
+        setLoading(true);
+        try {
+            await api.login(email, password);
+            const me = await api.me();
+            setUser(me);
+            setMsg("ログイン成功！");
+        } catch (e) {
+            setMsg(`ログイン失敗：${e}`);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+
+    return(
+        <>
+            <div style={{display:"grid", gap:8, maxWidth:420}}>
+                <h3>ログイン</h3>
+                <input placeholder="メール" value={email} onChange={e=>setEmail(e.target.value)} />
+                <input placeholder="パスワード" type="password" value={password} onChange={e=>setPassword(e.target.value)} />
+                <button onClick={onLogin} disabled={loading}>{loading ? "処理中..." : "ログイン"}</button>
+                {msg && <p>{msg}</p>}
+                {user?.id}{user?.email}{user?.display_name}
+            </div>
+        </>
+    )
+}
