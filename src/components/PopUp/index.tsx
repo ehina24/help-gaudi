9db1def485c156e413e8ddf45bf7f9ad5b21e48d
Tanikano <2250361@ecc.ac.jp>
2025-12-05 11:07:00 +0900

    add:ドットフォント追加

--- src/components/PopUp/index.tsx
+++ src/components/PopUp/index.tsx
@@ -1,13 +1,18 @@
 import './popup.css';
 
 
+
+
+
 export default function Popup(){
     return(
         <div className="messageBox">
             <p>えぶりでいホスト読みましたか？今週も面白かったですね！</p>
 
+
             <div className="response">
-                <div>
+
+                <div className="button-wrapper">
                     <div>
                         <div className="triangle"></div>
                         <button>はい</button>
@@ -17,6 +22,8 @@ export default function Popup(){
                         <button>いいえ</button>
                     </div>
                 </div>
+
+
             </div>
 
         </div>
